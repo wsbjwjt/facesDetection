334b5cba40ec25523b9919b9969005dafc86f687
wsbjwjt <wsbjwjt@gmail.com>
2018-08-12 22:26:03 +0800

    Adding organization module.

---
+++ apps/organization/models.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+__author__ = 'Eric'
+__date__ = '2018/8/11 22:00'
+
+from django.db import models
+
+
+# Create your models here.
+class organization(models.Model):
+    pass
