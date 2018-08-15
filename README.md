ea59ec8cf380724fe5aaa780a94105011eef4f59
Daviloper <805288270@qq.com>
2018-08-15 11:22:16 +0800

    提交测试

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # facesDetection
-facesDetection
+facesDetection...
--- facesDetection/settings.py
+++ facesDetection/settings.py
@@ -128,7 +128,6 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
